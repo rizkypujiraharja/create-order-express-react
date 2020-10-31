f16915f4bfdfb6a269585e63c1fdcdf2d109855a
Rizky Puji Raharja <rizky.rsj@gmail.com>
2020-10-31 18:21:05 +0700

    Payment App

---
+++ payment-app/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/', async (req, res) => {
+    res.send("Payment App")
+})
+
+app.post('/check-payment', async (req, res) => {
+    res.send({
+        orderId: req.body.orderId,
+        isPaid: Math.random() >= 0.5
+    })
+})
+
+app.listen(port, async () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
